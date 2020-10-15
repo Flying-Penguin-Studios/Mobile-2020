3235a4b9337b749a45537f7982817bf221f67000
Bruno <bruno12pessoa@gmail.com>
2020-10-14 23:03:43 -0300

    Prototipo?
    Possivel Build do Prototipo

---
+++ Mobile 2020/Assets/Scripts/IlanSprites.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IlanSprites : MonoBehaviour
+{
+    Image Image_Sprite;
+    SpriteRenderer Render_Sprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Image_Sprite = GetComponent<Image>();
+        Render_Sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Image_Sprite.sprite = Render_Sprite.sprite;
+    }
+}
